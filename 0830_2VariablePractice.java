cbb7e6a005757b355fc91024fd1c2be3d8660c08
LucyKuo <yujuko@gmail.com>
2018-08-30 15:00:05 -0400

    -

---
+++ 0830_2VariablePractice.java
@@ -0,0 +1,15 @@
+
+
+package printing;
+
+public class VariablePractice {
+
+	String name = "Lucy";
+	char middleInitial = 'K';
+	int age = 26;
+	double yearSwitchCompany = 1.7;
+	boolean eligibleForRemoteWork = true;
+	System.out.println("Hello "+name+"!");
+	S
+			
+}
