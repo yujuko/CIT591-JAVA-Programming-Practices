2b35955436a5fd8f1d61b882cddb490e93f26dc0
LucyKuo <yujuko@gmail.com>
2018-09-20 12:51:58 -0400

    -

---
+++ 0904_simpleCalculator/static methods/CalculatorTester_static.java
@@ -0,0 +1,14 @@
+package objectPractice;
+
+public class CalculatorTester {
+
+	public static void main (String[]args) {
+		
+//        SimpleCalculator calculator = new SimpleCalculator();
+        
+        SimpleCalculator.add(5,5);
+        //with a static method, we can call the method in that class
+		
+
+	}
+}
